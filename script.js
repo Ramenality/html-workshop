044c4f8b612c617859595f3233f5d364f9ab6bdd
ramon <ramonmathias000@gmail.com>
2025-02-05 15:34:36 +0200

    valmis leht

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('myButton').addEventListener('click', function() {
+    alert('Makse aadressile 1Lbcfr7sAHTD9CgdQo3HTMTkV8LK4ZnX71 0.0000001BTC muidu saadame teile ukse taha KIVID!!!!!');
+});
